7e292c9e3cca8e59f245b30aa3f1433d04cdcf0a
Manuelmillang <manuelmillang@esliceu.net>
2024-03-07 17:45:27 +0100

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// Inputs:
+const multiplicando = document.querySelector("#multiplicando");
+const taulaMulti = document.querySelector("#taulaMulti");
+const colorAleatori = document.querySelector("#colorAleatori");
+const principal = document.querySelector("#principal");
+const numero = document.querySelector("#numero");
+const resultatArray = document.querySelector("#resultatArray");
+
+// Botons:
+const btnTaulaMulti = document.querySelector("#btnTaulaMulti");
+const btnNetejaEx1 = document.querySelector("#netejaEx1");
+const btnStartStop = document.querySelector("#btnStartStop");
+const btnNetejaEx2 = document.querySelector("#netejaEx2");
+const btnAfegir = document.querySelector("#btnAfegir");
+const netejaEx3 = document.querySelector("#netejaEx3");
+
+//Events:
+btnTaulaMulti.onclick = generaTabla;
+btnNetejaEx1.onclick = netejaEx1;
+btnStartStop.onclick = startStopGame;
+btnNetejaEx2.onclick = netejaEx2;
+btnAfegir.onclick = afegeixElementArray;
+netejaEx3.onclick = netejaArray;
+
+/* Exercici 1*/
+function generaTabla() {
+
+}
+
+function netejaEx1() {
+    
+}
+
+
+/* Exercici 2*/
+let intervalo;
+function startStopGame(){
+  
+}
+
+let color = "";
+
+function startAleatori() {
+ 
+}
+
+function stopAleatori() {
+
+}
+
+function netejaEx2() {
+
+}
+
+
+/* Exercici 3*/
+let array = [];
+
+function afegeixElementArray() {
+   
+}
+
+function netejaArray() {
+  
+}
